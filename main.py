d69520cc43b0833bb65ca24fe49858c6d569dd6b
rossum <rossum@tuta.io>
2021-05-05 20:10:07 +0200

    black

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Script for backing up database from a Docker container to Synology.
+"""
+
 import datetime
 import os
 
@@ -6,28 +10,39 @@ from synology_api import filestation
 
 load_dotenv()
 
-DOCKER_CONTAINER = os.environ['DOCKER_CONTAINER']
-FILE_PREFIX = os.environ['FILE_PREFIX']
-POSTGRES_USER = os.environ['POSTGRES_USER']
-SYNOLOGY_PASSWORD = os.environ['SYNOLOGY_PASSWORD']
-SYNOLOGY_PATH = os.environ['SYNOLOGY_PATH']
-SYNOLOGY_PORT = os.environ['SYNOLOGY_PORT']
-SYNOLOGY_USER = os.environ['SYNOLOGY_USER']
-SYNOLOGY_HOST = os.environ['SYNOLOGY_HOST']
+DOCKER_CONTAINER = os.environ["DOCKER_CONTAINER"]
+FILE_PREFIX = os.environ["FILE_PREFIX"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+SYNOLOGY_PASSWORD = os.environ["SYNOLOGY_PASSWORD"]
+SYNOLOGY_PATH = os.environ["SYNOLOGY_PATH"]
+SYNOLOGY_PORT = os.environ["SYNOLOGY_PORT"]
+SYNOLOGY_USER = os.environ["SYNOLOGY_USER"]
+SYNOLOGY_HOST = os.environ["SYNOLOGY_HOST"]
+
 
 def get_date():
-    return datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    """Get date in ISO format."""
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 
 def main():
-    os.chdir('/tmp')
-    
-    filename = '{}_{}.sql'.format(FILE_PREFIX, get_date())
+    """Main entrypoint of the script."""
+    os.chdir("/tmp")
 
-    os.system('docker exec -t {} pg_dumpall -c -U {} > {}'.format(DOCKER_CONTAINER, POSTGRES_USER, filename))
+    filename = "{}_{}.sql".format(FILE_PREFIX, get_date())
 
-    synology_server = filestation.FileStation(SYNOLOGY_HOST, SYNOLOGY_PORT, SYNOLOGY_USER, SYNOLOGY_PASSWORD, secure=True)
+    os.system(
+        "docker exec -t {} pg_dumpall -c -U {} > {}".format(
+            DOCKER_CONTAINER, POSTGRES_USER, filename
+        )
+    )
+
+    synology_server = filestation.FileStation(
+        SYNOLOGY_HOST, SYNOLOGY_PORT, SYNOLOGY_USER, SYNOLOGY_PASSWORD, secure=True
+    )
 
     synology_server.upload_file(SYNOLOGY_PATH, filename)
+
 
 if __name__ == "__main__":
     main()
--- restore.py
+++ restore.py
@@ -1,4 +1,7 @@
-import datetime
+"""
+Script for restoring database from Synology to a Docker container.
+"""
+
 import os
 
 from dotenv import load_dotenv
@@ -6,29 +9,42 @@ from synology_api import filestation
 
 load_dotenv()
 
-DOCKER_CONTAINER = os.environ['DOCKER_CONTAINER']
-FILE_PREFIX = os.environ['FILE_PREFIX']
-POSTGRES_USER = os.environ['POSTGRES_USER']
-SYNOLOGY_PASSWORD = os.environ['SYNOLOGY_PASSWORD']
-SYNOLOGY_PATH = os.environ['SYNOLOGY_PATH']
-SYNOLOGY_PORT = os.environ['SYNOLOGY_PORT']
-SYNOLOGY_USER = os.environ['SYNOLOGY_USER']
-SYNOLOGY_HOST = os.environ['SYNOLOGY_HOST']
+DOCKER_CONTAINER = os.environ["DOCKER_CONTAINER"]
+FILE_PREFIX = os.environ["FILE_PREFIX"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+SYNOLOGY_PASSWORD = os.environ["SYNOLOGY_PASSWORD"]
+SYNOLOGY_PATH = os.environ["SYNOLOGY_PATH"]
+SYNOLOGY_PORT = os.environ["SYNOLOGY_PORT"]
+SYNOLOGY_USER = os.environ["SYNOLOGY_USER"]
+SYNOLOGY_HOST = os.environ["SYNOLOGY_HOST"]
+
 
 def main():
-    os.chdir('/tmp')
+    """Main entrypoint of the script."""
+    os.chdir("/tmp")
 
-    synology_server = filestation.FileStation(SYNOLOGY_HOST, SYNOLOGY_PORT, SYNOLOGY_USER, SYNOLOGY_PASSWORD, secure=True)
+    synology_server = filestation.FileStation(
+        SYNOLOGY_HOST, SYNOLOGY_PORT, SYNOLOGY_USER, SYNOLOGY_PASSWORD, secure=True
+    )
 
-    results = synology_server.search_start(folder_path=SYNOLOGY_PATH, pattern='fin_', extension='sql')
+    results = synology_server.search_start(
+        folder_path=SYNOLOGY_PATH, pattern="fin_", extension="sql"
+    )
 
-    id = results.split('your id is: ')[1].replace('"', '')
+    task_id = results.split("your id is: ")[1].replace('"', "")
 
-    file_info = synology_server.get_search_list(task_id=id, limit=1, sort_by='crtime', sort_direction='desc')['data']['files'][0]
+    file_info = synology_server.get_search_list(
+        task_id=task_id, limit=1, sort_by="crtime", sort_direction="desc"
+    )["data"]["files"][0]
 
-    synology_server.get_file(file_info['path'], 'download')
+    synology_server.get_file(file_info["path"], "download")
 
-    os.system('cat {} | docker exec -i {} psql -U {}'.format(file_info['name'], DOCKER_CONTAINER, POSTGRES_USER))
+    os.system(
+        "cat {} | docker exec -i {} psql -U {}".format(
+            file_info["name"], DOCKER_CONTAINER, POSTGRES_USER
+        )
+    )
+
 
 if __name__ == "__main__":
     main()
